b9ddeec2e11cb0cc231339eb23650f67d8e52a45
maidou <maidou@anybeen.net>
2016-04-16 09:28:16 +0800

    待删除的测试版本

--- app/src/main/java/com/dyzs/testringringview/CloudTagAdapter.java
+++ app/src/main/java/com/dyzs/testringringview/CloudTagAdapter.java
@@ -23,13 +23,8 @@ import java.util.Random;
 public class CloudTagAdapter extends BaseAdapter {
     private Context mContext;
     private List<String> mList;
-    private int mCurTimeMillis = -100;
-    private int mPosition = 0;
-
-    private int millis_1 = -200;
-    private int millis_2 = 0;
-    private int millis_3 = 200;
-    private int millis_4 = 300;
+    private int mCurTimeMillis = -200;
+    private int mPosition = -1;
 
     public CloudTagAdapter(Context context, ArrayList<String> list) {
         this.mContext = context;
@@ -53,6 +48,29 @@ public class CloudTagAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        System.out.println("pos" + position);
+        mPosition += 1;
+        int row = mPosition / 4;
+        if (mPosition < 4 * (row + 1) && ((row) % 2 == 1)) {
+            mCurTimeMillis = 200;
+        } else {
+            mCurTimeMillis = 0;
+        }
+//        mPosition += 1;
+//        mCurTimeMillis += 100;
+//        if (mPosition % 2 == 0) {
+//            mCurTimeMillis = 50;
+//        }
+//        if (mPosition < 4) {
+//            mCurTimeMillis += 200;
+//        } else {
+//            if ((mPosition) == (row * 4)) {
+//                mCurTimeMillis = 100;
+//            } else {
+//                mCurTimeMillis = mCurTimeMillis + 200;
+//            }
+//        }
+
 
         ViewHolder viewHolder;
         int color = mList.get(position).hashCode() | 0xF0000000 & 0xFFF5F5F5;
@@ -78,37 +96,12 @@ public class CloudTagAdapter extends BaseAdapter {
         viewHolder.ll_cloud_tag_ring.setProgress(new Random().nextInt(100) + 1);// 设置当前进度
         viewHolder.tv_cloud_tag_name.setText(mList.get(position));
         viewHolder.tv_cloud_tag_name.setTextColor(color);
-//      为什么这个才是第四列
-//        if (mPosition == (1 + 4*((mPosition - 1) / 4))) {
-//            millis_1 += 200;
-//            actAnimation1(convertView, millis_1);
-//        }
-//      为什么这个才是第一列
-        if (mPosition == (2 + 4*((mPosition - 1) / 4))) {
-            millis_2 += 200;
-            actAnimation1(convertView, millis_2);
-        }
-//      为什么这个才是第二列
-        if (mPosition == (3 + 4*((mPosition - 1) / 4))) {
-            millis_3 += 200;
-            actAnimation1(convertView, millis_3);
-        }
-//      为什么这个才是第三列
-        if (mPosition == (4 + 4*((mPosition - 1) / 4))) {
-            millis_4 += 200;
-            actAnimation1(convertView, millis_4);
-        }
-//
-        mPosition += 1;
 
 
-//        mPosition += 1;
-//        mCurTimeMillis += 100;
-//        if (mPosition % 2 == 0) {
-//            mCurTimeMillis = 50;
-//        }
-//
-//        actAnimation(convertView);
+        System.out.println("mCurTimeMillis:" + mCurTimeMillis);
+        actAnimation(convertView);
+
+
         return convertView;
 
 //        121212的方法
@@ -183,7 +176,7 @@ public class CloudTagAdapter extends BaseAdapter {
         return rrv;
     }
 
-    private void actAnimation(View convertView) {
+    private synchronized void actAnimation(View convertView) {
 //        ViewHelper.setTranslationX(convertView, -100f);
 //        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator()).translationX(0f).setDuration(300).setStartDelay(mCurTimeMillis);
         ViewHelper.setScaleX(convertView, 0.0f);    // 初始缩放为0.1f
@@ -202,41 +195,5 @@ public class CloudTagAdapter extends BaseAdapter {
 //        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).alpha(1f).setDuration(600).setStartDelay(mCurTimeMillis + 100);
 //        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleX(1f).setDuration(600).setStartDelay(mCurTimeMillis + 100);
 //        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleY(1f).setDuration(600).setStartDelay(mCurTimeMillis + 100);
-    }
-
-    private void actAnimation1(View convertView, int millis) {
-        ViewHelper.setScaleX(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setScaleY(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setAlpha(convertView, 0.0f);     // 初始透明为0.1f
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).alpha(1f).setDuration(700).setStartDelay(millis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleX(1f).setDuration(700).setStartDelay(millis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleY(1f).setDuration(700).setStartDelay(millis);
-    }
-
-    private void actAnimation2(View convertView) {
-        ViewHelper.setScaleX(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setScaleY(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setAlpha(convertView, 0.0f);     // 初始透明为0.1f
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).alpha(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleX(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleY(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-    }
-
-    private void actAnimation3(View convertView) {
-        ViewHelper.setScaleX(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setScaleY(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setAlpha(convertView, 0.0f);     // 初始透明为0.1f
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).alpha(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleX(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleY(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-    }
-
-    private void actAnimation4(View convertView) {
-        ViewHelper.setScaleX(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setScaleY(convertView, 0.0f);    // 初始缩放为0.1f
-        ViewHelper.setAlpha(convertView, 0.0f);     // 初始透明为0.1f
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).alpha(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleX(1f).setDuration(700).setStartDelay(mCurTimeMillis);
-        ViewPropertyAnimator.animate(convertView).setInterpolator(new OvershootInterpolator(2)).scaleY(1f).setDuration(700).setStartDelay(mCurTimeMillis);
     }
 }
